54ff1fbc35f522f2a72eef4c73673a18051bdd2e
CoachLuck <ajiellodev@gmail.com>
2021-01-16 17:09:23 -0700

    add core to classpath

---
+++ yaml-config/src/test/java/TestYamlConfig.java
@@ -0,0 +1,11 @@
+import io.coachluck.yaml.file.YamlConfiguration;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+public class TestYamlConfig {
+
+    @Test
+    public void testYaml() {
+        YamlConfiguration yaml = new YamlConfiguration();
+    }
+}
